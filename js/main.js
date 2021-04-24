b47c51baa3908011e33cf54b1c14d3e4746fb551
Perry Raleigh <perry.raleigh@gmail.com>
2021-04-24 02:31:12 -0600

    Yet again, again...

--- js/main.js
+++ js/main.js
@@ -5,13 +5,11 @@ const links = [
     }
   ]
 
-  var weeksList = { 
-    buildWeeks: function() {
-      var oList = document.getElementById("weeksOL");
-      for (var i = 0; i < links.length; i++) {
-        oList.innerHTML += "<li><a href='" + links[i].url +"</a></li>";
-    }
+    function buildWeeks() {
+    var oList = document.getElementById("weeksOL");
+    for (var i = 0; i < links.length; i++) {
+      oList.innerHTML += "<li><a href='" + links[i].url +"</a></li>";
   }
 }
 
-document.getElementById("myWork").innerHTML = weeksList;
+document.getElementById("myWork").innerHTML = buildWeeks;
