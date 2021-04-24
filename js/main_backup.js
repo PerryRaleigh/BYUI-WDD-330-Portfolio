ddc122fd4346905138caf73accf7320ec4d79bfc
Perry Raleigh <perry.raleigh@gmail.com>
2021-04-24 03:05:45 -0600

    last try tonight

---
+++ js/main_backup.js
@@ -0,0 +1,14 @@
+const links = [
+    {
+      label: "Week01 notes",
+      url: "week01/index.html"
+    }
+  ]
+
+    function buildWeeks() {
+    var oList = document.getElementById("weeksOL");
+    for (var i = 0; i < links.length; i++) {
+      oList.innerHTML += "<li><a href='" + links[i].url +"</a></li>";
+  }
+}
+buildWeeks();
