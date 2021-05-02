7c6d490e962d79a9048cd633f23b86fa3740f67e
Perry Raleigh <perry.raleigh@gmail.com>
2021-05-01 23:40:01 -0600

    Final wrap up of week 03

--- js/main.js
+++ js/main.js
@@ -10,6 +10,50 @@ const links = [
   {
     label: "Week03 notes",
     url: "week03/index.html"
+  },
+  {
+    label: "Week04 notes",
+    url: "week04/index.html"
+  },
+  {
+    label: "Week05 notes",
+    url: "week05/index.html"
+  },
+  {
+    label: "Week06 notes",
+    url: "week06/index.html"
+  },
+  {
+    label: "Week07 notes",
+    url: "week07/index.html"
+  },
+  {
+    label: "Week08 notes",
+    url: "week08/index.html"
+  },
+  {
+    label: "Week09 notes",
+    url: "week09/index.html"
+  },
+  {
+    label: "Week10 notes",
+    url: "week10/index.html"
+  },
+  {
+    label: "Week11 notes",
+    url: "week11/index.html"
+  },
+  {
+    label: "Week12 notes",
+    url: "week12/index.html"
+  },
+  {
+    label: "Week13 notes",
+    url: "week13/index.html"
+  },
+  {
+    label: "Week14 notes",
+    url: "week14/index.html"
   }
 ]
 
